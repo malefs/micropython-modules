9f1b7caa18e3483db31aabe1627059b738ffea09
Brandon Gant <bgant@protonmail.com>
2019-03-21 10:38:11 -0500

    sleep and reset on any error

--- esp32/main.py
+++ esp32/main.py
@@ -2,30 +2,35 @@ import utime
 print('main.py: Press CTRL+C to drop to REPL...')
 utime.sleep(3)
 
-import btree
-import client
-import machine
-import mqtt
-import tmp102
+try:
+    import btree
+    import client
+    import machine
+    import mqtt
+    import tmp102
 
-unique_id = str(client.id())
+    unique_id = str(client.id())
 
-f = open('key_store.db', 'r+b')
-db = btree.open(f)
+    f = open('key_store.db', 'r+b')
+    db = btree.open(f)
 
-broker = db[b'mqtt_broker'].decode('utf-8')
-topic = 'devices/' + unique_id
+    broker = db[b'mqtt_broker'].decode('utf-8')
+    topic = 'devices/' + unique_id
 
-timestamp = str(utime.time())   # Epoch UTC
-mqtt.publish(broker, topic + '/time', timestamp) 
+    timestamp = str(utime.time())   # Epoch UTC
+    mqtt.publish(broker, topic + '/time', timestamp) 
 
-temp = str(round(tmp102.read_temp('F'), 1))
-mqtt.publish(broker, topic + '/temp', temp)
+    temp = str(round(tmp102.read_temp('F'), 1))
+    mqtt.publish(broker, topic + '/temp', temp)
 
-db[str(timestamp)] = str(temp) 
-db.flush()
-db.close()
+    db[str(timestamp)] = str(temp) 
+    db.flush()
+    db.close()
 
-print(timestamp, temp)
-utime.sleep(1)  # Give UART time to print text before going to sleep
-machine.deepsleep(300000)    # Reset on Wake
+    print(timestamp, temp)
+    utime.sleep(1)  # Give UART time to print text before going to sleep
+    machine.deepsleep(300000)    # Reset on Wake
+
+except:
+    print('Error... sleeping again')
+    machine.deepsleep(300000) 
