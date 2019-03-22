371a5f91a73c2737d35aa07b065e6fa9db5961a5
Brandon Gant <bgant@protonmail.com>
2019-03-22 15:52:30 -0500

    generates key_store.txt that ampy can get

--- esp32/key_store.py
+++ esp32/key_store.py
@@ -10,25 +10,36 @@ def update():
     except OSError:
         f = open(file, 'w+b')
     db = btree.open(f)
-
     db[b'ssid_name'] = input('Enter WiFi SSID: ')
     db[b'ssid_pass'] = input('Enter WiFi password: ')
     db[b'mqtt_broker'] = input('Enter the MQTT Server IP: ')
-
     db.flush()
     print("%s, %s, and %s added to %s file" % (db[b'ssid_name'].decode('utf-8'), db[b'ssid_pass'].decode('utf-8'), db[b'mqtt_broker'].decode('utf-8'), file))
     db.close()
 
 
+# This just prints to the screen. Output is not usable in scripts.
 def dump():
-
     f = open(file, 'r+b')
     db = btree.open(f)
-
     for key in db:
         print(key.decode('utf-8'), db[key].decode('utf-8'))
-
     db.close()
+
+
+# This returns a Dictionary list that is usable in scripts.
+def dict():
+    f = open(file, 'r+b')
+    db = btree.open(f)
+    list = {}
+    for key in db:
+        pair = {key.decode('utf-8'): db[key].decode('utf-8')}
+        list.update(pair)
+    db.close()
+    # Text file can be retrieved with: ampy -p /dev/ttyUSB0 get key_store.txt
+    with open('key_store.txt', 'wt') as text:
+        text.write(str(list))
+    return list
 
 
 def remove():
