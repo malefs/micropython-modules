67ff08235c00821c6bf49e0bc7e02fec79113930
Brandon Gant <bgant@protonmail.com>
2019-03-28 20:55:16 -0500

    power changes logged locally in key_store

--- esp32/main_power.py
+++ esp32/main_power.py
@@ -1,5 +1,6 @@
 
 from machine import Pin
+import btree
 import client
 import mqtt
 import utime
@@ -8,12 +9,15 @@ import utime
 # Between resistors, a 2.5V wire to Pin 4
 pin = Pin(4, Pin.IN)
 
+# Get Unique Client ID
 unique_id = str(client.id())
 
+# Get MQTT Broker IP Address
 f = open('key_store.db', 'r+b')
 db = btree.open(f)
-
 broker = db[b'mqtt_broker'].decode('utf-8')
+db.close()
+
 topic = 'devices/' + unique_id
 
 # Initialize variables and assume power is on
@@ -24,7 +28,19 @@ print('Monitoring Power...')
 while True:
     current_power_status = pin.value()
     if current_power_status != last_power_status:
-        mqtt.publish(broker, topic + '/power/timestamp', str(utime.time()))  # Epoch UTC
+        # Send changes to MQTT server
+        timestamp = utime.time()
+        mqtt.publish(broker, topic + '/power/timestamp', str(timestamp))  # Epoch UTC
         mqtt.publish(broker, topic + '/power/value', str(current_power_status))
+        
+        # Store values locally
+        f = open('key_store.db', 'r+b')
+        db = btree.open(f)
+        db[str(timestamp)] = str(current_power_status) 
+        db.flush()
+        db.close()
+
         last_power_status = current_power_status
+
     utime.sleep(1) 
+
