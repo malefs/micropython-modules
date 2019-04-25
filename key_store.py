75b978f68f08c0a497209282976b91a5e5e6f0ee
Brandon Gant <bgant@protonmail.com>
2019-04-25 10:54:02 -0500

    reworked key_store.py

--- key_store.py
+++ key_store.py
@@ -5,19 +5,27 @@
 #
 # Usage:
 #    import key_store
-#    key_store.update()   <-- Creates key_store.db if it does not exist
-#    key_store.print()    <-- Prints contents of key_store.db to screen
-#    key_store.text()     <-- Dumps contents of key_store.db to key_store.txt which ampy can retrieve
-#    key_store.remove()   <-- Removes key_store.db file
+#    key_store.init()              <-- Creates key_store.db if it does not exist
+#    key_store.set('key','value')  <-- Sets key/value in database
+#    key_store.get('key')          <-- Gets value from database
+#    key_store.dumptext()          <-- Prints contents of key_store.db to screen
+#    key_store.dumpfile()          <-- Dumps contents of key_store.db to key_store.txt which ampy can retrieve
+#    key_store.remove()            <-- Removes key_store.db file
 #
-# This script just provides a way to log everything locally if needed.
+# This script keeps private settings out of github and also logs everything locally if needed.
 #
 
 import btree
 
 file = 'key_store.db'
 
-def update():
+try:
+    f = open(file, 'r+b')
+except OSError:
+    print('WARNING: No %s on disk' % file)    
+
+# Create a new key_store.db database or update config settings
+def init():
     try:
         f = open(file, 'r+b')
     except OSError:
@@ -30,16 +38,31 @@ def update():
     print("%s, %s, and %s added to %s file" % (db[b'ssid_name'].decode('utf-8'), db[b'ssid_pass'].decode('utf-8'), db[b'mqtt_broker'].decode('utf-8'), file))
     db.close()
 
+# Added new key/value pairs to key_store.db
+def set(key,value):
+    f = open(file, 'r+b')
+    db = btree.open(f)
+    db[key] = value
+    db.flush()
+    db.close()
+
+# Retrieve data from key_store.db
+def get(key):
+    f = open(file, 'r+b')
+    db = btree.open(f)
+    return db[key].decode('utf-8')
+    db.close()
 
 # This just prints to the screen which is not usable in scripts.
-def dump():
+def dumptext():
     f = open(file, 'r+b')
     db = btree.open(f)
     for key in db:
         print(key.decode('utf-8'), db[key].decode('utf-8'))
     db.close()
 
-def text():
+# Allows you to download local data: ampy -p /dev/ttyUSB0 get key_store.txt 
+def dumpfile():
     f = open(file, 'r+b')
     db = btree.open(f)
     with open('key_store.txt', 'wt') as text:
@@ -47,7 +70,9 @@ def text():
             pair = "{}:{}\n".format(key.decode('utf-8'), db[key].decode('utf-8'))
             text.write(pair)
     db.close()
+    print('key_store.txt created')
 
+# Removes key_store.db
 def remove():
     import uos
     uos.remove(file)
