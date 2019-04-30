121358516a1d2bb27e5b0b19fa0773092c0d8279
Brandon Gant <bgant@protonmail.com>
2019-04-30 14:28:08 -0500

    broke into two parts

--- mqtt_as/main_setup.sh
+++ mqtt_as/main_setup.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
+if [ $1 == "erase" ]
+then
 esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
 sleep 7
 esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 ~/Downloads/esp32-bluetooth.bin 
+fi
 
-echo "Waiting 20 seconds for reboot and reconnection..."
-sleep 20
+if [ $1 == "ampy" ]
+then
+AMPY="ampy --port /dev/ttyUSB0 --baud 115200 --delay 1"
 
 echo "Creating uasyncio directory..."
 ampy -p /dev/ttyUSB0 mkdir uasyncio
 
 echo "Loading __init__.py..."
-ampy -p /dev/ttyUSB0 put ~/source/micropython-modules/mqtt_as/uasyncio/__init__.py uasyncio/__init__.py
+$AMPY put ~/source/micropython-modules/mqtt_as/uasyncio/__init__.py uasyncio/__init__.py
 
 echo "Loading core.py..."
-ampy -p /dev/ttyUSB0 put ~/source/micropython-modules/mqtt_as/uasyncio/core.py uasyncio/core.py
+$AMPY put ~/source/micropython-modules/mqtt_as/uasyncio/core.py uasyncio/core.py
 
 echo "Loading mqtt_as.py..."
-ampy -p /dev/ttyUSB0 put ~/source/micropython-modules/mqtt_as/mqtt_as.py 
+$AMPY put ~/source/micropython-modules/mqtt_as/mqtt_as.py 
 
 echo "Loading key_store.py..."
-ampy -p /dev/ttyUSB0 put ~/source/micropython-modules/key_store.py 
+$AMPY put ~/source/micropython-modules/key_store.py 
 
 echo "Loading sparkfun_esp32_blue_led.py..."
-ampy -p /dev/ttyUSB0 put ~/source/micropython-modules/sparkfun_esp32/sparkfun_esp32_blue_led.py 
+$AMPY put ~/source/micropython-modules/sparkfun_esp32/sparkfun_esp32_blue_led.py 
 
 #echo "Loading main_power.py..."
-#ampy -p /dev/ttyUSB0 put ~/source/micropython-modules/mqtt_as/main_power.py /main.py
+#$AMPY put ~/source/micropython-modules/mqtt_as/main_power.py /main.py
 
 echo "Manually run the following commands to initialize settings:"
 echo "   screen /dev/ttyUSB0 115200"
@@ -34,4 +38,4 @@ echo "   import key_store"
 echo "   key_store.init()"
 echo ""
 echo "Manually add your own main.py file"
-
+fi
