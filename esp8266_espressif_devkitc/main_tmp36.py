ef558a8afbf90656fada0c5e5d10d2037fdd6ed5
Brandon Gant <bgant@protonmail.com>
2019-12-13 16:51:39 -0600

    works for single data send

--- esp8266_espressif_devkitc/main_tmp36.py
+++ esp8266_espressif_devkitc/main_tmp36.py
@@ -13,21 +13,24 @@ import key_store
 from machine import ADC
 from time import sleep
 
+# Get ThingSpeak API Key
+f = open('key_store.db', 'r+b')
+db = btree.open(f)
+thingspeak_api_key = db[b'thingspeak_api_key'].decode('utf-8')
+db.close()
+
+server = 'api.thingspeak.com'
+
+
 def main():
-    adc = ADC(0)
+    # Read the Temperature
     tempf = read_temp()
-    print('Current Temp:', tempf)
 
-    # Get ThingSpeak API Key
-    f = open('key_store.db', 'r+b')
-    db = btree.open(f)
-    thingspeak_api_key = db[b'thingspeak_api_key'].decode('utf-8')
-    db.close()
-
-    server = 'api.thingspeak.com'
+    # Create the HTTPS GET Request string
     get_request = 'GET /update?api_key=' + thingspeak_api_key + '&field1=' + tempf + ' HTTP/1.0\r\n\r\n'
-    get_request = str.encode(get_request)  # Convert type str to bytes
+    get_request = str.encode(get_request)  # Convert Type str to bytes
 
+    # Send the Data to ThingSpeak
     print('Sending Temp %s to %s' % (tempf, server))
     send_data(server, get_request)
 
@@ -40,32 +43,36 @@ def read_temp():
     temp = round(temp)        # Round to remove decimals
     temp = str(temp)          # Convert to string to put into URL field1
     return temp
-    #return str(round(((adc.read()*9/5/10) + 32)*1.07))
 
 
 def send_data(server, get_request, use_stream=True):
     s = socket.socket()
 
     ai = socket.getaddrinfo(server, 443)
-    print("Address infos:", ai)
+    #print("Address infos:", ai)
     addr = ai[0][-1]
 
-    print("Connect address:", addr)
+    print("DNS Response:", addr)
     s.connect(addr)
 
     s = ssl.wrap_socket(s)
-    print(s)
+    #print(s)
 
     if use_stream:
         # Both CPython and MicroPython SSLSocket objects support read() and
         # write() methods.
         s.write(get_request)  # Must be bytes instead of string
-        print(s.read(4096))
+        response_bytes = s.read(4096)
     else:
         # MicroPython SSLSocket objects implement only stream interface, not
         # socket interface
         s.send(get_request)   # Must be bytes instead of string
-        print(s.recv(4096))
+        response_bytes = s.recv(4096)
+
+    response_text = response_bytes.decode()
+    #print(response_text)
+    status = [ line for line in response_text.split('\r\n') if "Status" in line ]
+    print('HTTPS', status[0])
 
     s.close()
 
