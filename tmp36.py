875fef0403ec7e3c541f1466342d26309b97b877
Brandon Gant <bgant@protonmail.com>
2019-12-19 16:46:15 -0600

    trying to use pure math

--- tmp36.py
+++ tmp36.py
@@ -8,7 +8,29 @@ from machine import ADC
 from uos import uname
 from time import sleep
 
-def read_temp(gpio_pin_number, scale='c', in_min=205, in_max=3465):
+def read_adc(gpio_pin_number=37):
+    # Find Temperature just using the math equations (appears to be off)
+    from machine import Pin
+    adc = ADC(Pin(gpio_pin_number)) # Pins 32-39 are valid
+    adc.atten(ADC.ATTN_6DB)         # 0-2V
+    adc.width(ADC.WIDTH_12BIT)      # 0-4095 values
+
+    # Instead of a single reading let's take the average of multiple readings
+    adc_sample = []
+    for x in range(50):
+        adc_sample.append(adc.read())
+        sleep(0.01)
+    remove_high_low = sorted(adc_sample)[10:-10]
+    average_adc = sum(remove_high_low) / len(remove_high_low)
+
+    millivolts = average_adc * (2000/4095)  # Example: 750mV = 1535.6 * (2000/4095)
+    temp_celsius = (millivolts - 500) / 10  # Example: 25C = (750mV - 500) / 10
+    temp_fahrenheit = (temp_celsius * 9/5) + 32
+
+    return average_adc, millivolts, temp_celsius, temp_fahrenheit
+
+
+def read_temp(gpio_pin_number, scale='c', in_min=205, in_max=3495):
     hardware = uname().sysname
     if 'esp32' in hardware:
         # Source: https://docs.micropython.org/en/latest/esp32/quickref.html#adc-analog-to-digital-conversion
@@ -19,7 +41,7 @@ def read_temp(gpio_pin_number, scale='c', in_min=205, in_max=3465):
 
         # TMP36 Voltage to ADC scale
         # Min  100mV at -40C is ADC  205
-        # Max 1750mV at 125C is ADC 3583 but using lower value to calibrate with TMP102 readings
+        # Max 1750mV at 125C is ADC 3583 / Using lower value to calibrate with TMP102 and La Crosse readings
         #      750mV at  25C is ADC 1536
 
         # TMP36 Temperature scale
@@ -40,8 +62,11 @@ def read_temp(gpio_pin_number, scale='c', in_min=205, in_max=3465):
             sleep(0.01)
         remove_high_low = sorted(adc_sample)[10:-10]
         average_adc = sum(remove_high_low) / len(remove_high_low)
+        average_temp = range_map(average_adc, in_min, in_max, out_min, out_max)
+        average_temp = round(average_temp, 1)
+       
+        return average_temp
 
-        return round(range_map(average_adc, in_min, in_max, out_min, out_max), 1)
 
     elif 'esp8266' in hardware:
         # I am using the range_map() function below with measured datapoints:
@@ -61,8 +86,10 @@ def read_temp(gpio_pin_number, scale='c', in_min=205, in_max=3465):
         temp = round(temp, 1)  # Rounding to one decimal place
         return temp            # Return Temperature
 
+
     else:
         print('unknown hardware')
+
 
 # Re-maps a number from one range to another.
 # Python equivalent to Arduino map() function.
