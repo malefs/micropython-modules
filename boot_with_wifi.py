c5f801c11ac726bc7c9c90740129ffb42ec27ca0
Brandon Gant <bgant@protonmail.com>
2019-12-20 14:24:55 -0600

    updated boot.py files

--- boot_with_wifi.py
+++ boot_with_wifi.py
@@ -1,41 +1,49 @@
-# SparkFun ESP32 Thing: https://www.sparkfun.com/products/13907
-# MicroPython:          https://docs.micropython.org/en/latest/
+# MicroPython: https://docs.micropython.org/en/latest/
 #
 # Brandon Gant
-# 2019-02-08
+# Created: 2019-02-08
+# Updated: 2019-12-20
 #
 # Source: https://github.com/micropython/micropython/tree/master/ports/esp32#configuring-the-wifi-and-using-the-board
 # Source: https://boneskull.com/micropython-on-esp32-part-1/
 # Source: https://docs.micropython.org/en/latest/library/network.WLAN.html
 #
 # Usage:
-#     /home/pi/.local/bin/ampy --port /dev/ttyUSB0 put boot.py
+#     $ pip3 install --user mpfshell
+#     $ mpfshell
+#     mpfs [/]> open ttyUSB0
+#     mpfs [/]> put boot_with_wifi.py boot.py
+#     mpfs [/]> put key_store.py
+#     mpfs [/]> repl
+#     >>> <Ctrl+] to exit repl>
+#
+# --OR--
+#
+#     ampy --port /dev/ttyUSB0 put boot_with_wifi.py boot.py
 #     screen /dev/ttyUSB0 115200
 #         (Ctrl+a Shift+k to kill screen connection)
 #
 
+from machine import reset
 import utime
 print('boot.py: Press CTRL+C to drop to REPL...')
 utime.sleep(3)
 
-import btree
-import esp
-import key_store
-import machine
-import micropython
-import network
-import ntptime
-import uos
-from time import sleep
-
 # Create exceptions (feedback) in cases where normal RAM allocation fails (e.g. interrupts)
-micropython.alloc_emergency_exception_buf(100)
+from micropython import alloc_emergency_exception_buf
+alloc_emergency_exception_buf(100)
 
-ssid_name = key_store.get('ssid_name')
-ssid_pass = key_store.get('ssid_pass')
+try:
+    import key_store
+    ssid_name = key_store.get('ssid_name')
+    ssid_pass = key_store.get('ssid_pass')
+except:
+    key_store.init()
+    reset()
 
 # Connect to WiFI
 def wlan_connect(ssid, password):
+    import network
     wlan = network.WLAN(network.STA_IF)
     counter = 0
     if not wlan.active() or not wlan.isconnected():
@@ -45,31 +53,49 @@ def wlan_connect(ssid, password):
         while not wlan.isconnected():
             counter += 1
             if counter > 60:
-                machine.reset()
-            sleep(1)
+                reset()
+            utime.sleep(1)
             pass
     print('WiFi DHCP: ', wlan.ifconfig()[0])
+    print()
 
 # Set RTC using NTP
 def ntp():
-    print('')
+    import ntptime
     ntptime.host = key_store.get('ntp_host')
     print("NTP Server:", ntptime.host)
     while utime.time() < 10000:  # Retry until clock is set
         ntptime.settime()
         utime.sleep(1)
     print('UTC Time:   {}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(*utime.localtime()))
-    print('')
+    print()
          
 
 # Suppress ESP debug messages in the REPL
 def no_debug():
-    esp.osdebug(None)
+    from esp import osdebug
+    osdebug(None)
+
+def mem_stats():
+    from esp import flash_size
+    from uos import statvfs
+    fs_stat = statvfs('/')
+    fs_size = fs_stat[0] * fs_stat[2]
+    fs_free = fs_stat[0] * fs_stat[3]
+    print('   Flash Size: {:5,}KB'.format(flash_size()/1024))
+    print('  File System: {:5,}KB'.format(fs_size/1024))
+    print('   Free Space: {:5,}KB'.format(fs_free/1024 - 2000))
+    print()
+
+def list_files():
+    print("List of files on this device:")
+    print('   %s' % '\n   '.join(map(str, sorted(uos.listdir('/')))))
+    print()
+
 
 no_debug()
 wlan_connect(ssid_name, ssid_pass)
-#ntp()
+ntp()
+mem_stats()
+list_files()
 
-print("List of files on this device:")
-print('   %s' % '\n   '.join(map(str, sorted(uos.listdir('/')))))
-print('')
