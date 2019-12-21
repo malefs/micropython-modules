23af05459de019a9fa41a03cc60c6c80d5a0cfe3
Brandon Gant <bgant@protonmail.com>
2019-12-20 18:58:54 -0600

    cleanup

--- key_store.py
+++ key_store.py
@@ -25,10 +25,12 @@ import btree
 
 file = 'key_store.db'
 
+# Check to see if file is on disk
 try:
     f = open(file, 'r+b')
 except OSError:
     print('WARNING: No %s on disk' % file)    
+
 
 # Create a new key_store.db database or update config settings
 def init():
@@ -37,13 +39,14 @@ def init():
     except OSError:
         f = open(file, 'w+b')
     db = btree.open(f)
-    db[b'ssid_name'] = input('Enter WiFi SSID: ')
-    db[b'ssid_pass'] = input('Enter WiFi password: ')
-    db[b'ntp_host']  = input('Enter NTP server: ')
-    db[b'mqtt_broker'] = input('Enter the MQTT Server IP: ')
+    db[b'ssid_name']   = input('Enter WiFi SSID: ')
+    db[b'ssid_pass']   = input('Enter WiFi password: ')
+    db[b'ntp_host']    = input('Enter NTP server: ')
+    db[b'mqtt_broker'] = input('Enter MQTT server: ')
     db.flush()
     #print("%s, %s, and %s added to %s file" % (db[b'ssid_name'].decode('utf-8'), db[b'ssid_pass'].decode('utf-8'), db[b'mqtt_broker'].decode('utf-8'), file))
     db.close()
+
 
 # Added new key/value pairs to key_store.db
 def set(key,value):
@@ -53,12 +56,14 @@ def set(key,value):
     db.flush()
     db.close()
 
+
 # Retrieve data from key_store.db
 def get(key):
     f = open(file, 'r+b')
     db = btree.open(f)
     return db[key].decode('utf-8')
     db.close()
+
 
 # This just prints to the screen which is not usable in scripts.
 def dumptext():
@@ -67,6 +72,7 @@ def dumptext():
     for key in db:
         print(key.decode('utf-8'), db[key].decode('utf-8'))
     db.close()
+
 
 # Allows you to download local data: ampy -p /dev/ttyUSB0 get key_store.txt 
 def dumpfile():
@@ -79,8 +85,10 @@ def dumpfile():
     db.close()
     print('key_store.txt created')
 
+
 # Removes key_store.db
 def remove():
     import uos
     uos.remove(file)
     print('%s removed' % file)
+
