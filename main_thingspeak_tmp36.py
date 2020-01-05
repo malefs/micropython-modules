a50b683926c1dbaeb4a298e710be375e11e47840
Brandon Gant <bgant@protonmail.com>
2020-01-05 14:03:32 -0600

    added client_id

--- main_thingspeak_tmp36.py
+++ main_thingspeak_tmp36.py
@@ -13,6 +13,12 @@ from time import sleep
 from sys import exit
 from machine import reset
 
+# Get Unique Machine ID
+from ubinascii import hexlify
+from machine import unique_id
+client_id = hexlify(unique_id()).decode('utf-8')  # String with Unique Client ID
+print('Client ID:', client_id)
+
 # Check hardware
 from uos import uname
 hardware = uname().sysname
@@ -25,18 +31,12 @@ else:
     print('Not test with', hardware)
     exit(1)
 
-# Get ThingSpeak API Key
-import btree
-f = open('key_store.db', 'r+b')
-db = btree.open(f)
-thingspeak_api_key = db[b'thingspeak_api_key'].decode('utf-8')
-db.close()
-
 #server = 'api.thingspeak.com'
 server = '192.168.7.100'
 #port = '44301' # HTTPS
 port = '8001'   # HTTP
 
+# ThingSpeak free tier limited to 15 seconds between data updates
 sleep_interval = 60   # Seconds
 periodic_reset = 360  # with 60 second sleep, reset every 6 hours (just in case)
 
@@ -54,24 +54,20 @@ def main():
         tempf = tmp36.read_temp(0,'F')
     print('Temperature Reading: %sF' % tempf)
 
-    # Create the HTTPS GET Request string
-    get_request = 'GET /update?api_key=' + thingspeak_api_key + '&field1=' + str(tempf) + ' HTTP/1.0\r\n\r\n'
-    get_request = str.encode(get_request)  # Convert Type str to bytes
-
-    # Send the Data to ThingSpeak
+    # Send the Data to Server
     print('Sending Data To: %s:%s' % (server, port))
     if 'esp32' in hardware:
         if '443' in port:
             transport = 'https://'
         else:
             transport = 'http://'
-        URL = transport + server + ':' + port + '/update?api_key=' + thingspeak_api_key + '&field1=' + str(tempf)
+        URL = transport + server + ':' + port + '/update?api_key=' + client_id + '&field1=' + str(tempf)
         r = urequests.get(URL)
         response_text = r.text
         status = str(r.status_code)
     elif 'esp8266' in hardware:
-        # Create the HTTPS GET Request string
-        get_request = 'GET /update?api_key=' + thingspeak_api_key + '&field1=' + str(tempf) + ' HTTP/1.0\r\n\r\n'
+        # Create the GET Request string
+        get_request = 'GET /update?api_key=' + client_id + '&field1=' + str(tempf) + ' HTTP/1.0\r\n\r\n'
         get_request = str.encode(get_request)  # Convert Type str to bytes
         response_text = http_client.send_data(server, get_request)
         #print(response_text)
@@ -87,7 +83,6 @@ def main():
         reset()
 
 
-# ThingSpeak free tier limited to 15 seconds between data updates
 counter = 0
 while True:
     try:
