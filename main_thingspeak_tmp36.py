0344a27a4372e2ce98184abfc968a0c9ca48120f
Brandon Gant <bgant@protonmail.com>
2020-01-08 10:41:49 -0600

    move server:port to key_store

--- main_thingspeak_tmp36.py
+++ main_thingspeak_tmp36.py
@@ -7,11 +7,12 @@
 # TMP36 temperature sensor with Vout connected to ADC pin
 #
 
+from machine import reset
+from time import sleep
+
 import key_store
 import AnalogDevices_TMP36 as tmp36
-from time import sleep
 from sys import exit
-from machine import reset
 
 # Get Unique Machine ID
 from ubinascii import hexlify
@@ -28,17 +29,23 @@ elif 'esp8266' in hardware:
     # No urequests in ESP8266 Micropython
     import http_client
 else:
-    print('Not test with', hardware)
+    print('Not tested with', hardware)
     exit(1)
 
-#server = 'api.thingspeak.com'
-server = '192.168.7.100'
-#port = '44301' # HTTPS
-port = '8001'   # HTTP
+# Get iot-api server:port from key_store.db
+import btree
+f = open('key_store.db', 'r+b')
+db = btree.open(f)
+server, port = db[b'iot-api'].decode('utf-8').split(':')
+
+if server == 'api.thingspeak.com':
+    f = open('key_store.db', 'r+b')
+    db = btree.open(f)
+    client_id = db[b'thingspeak_api_key'].decode('utf-8')
 
 # ThingSpeak free tier limited to 15 seconds between data updates
-sleep_interval = 60   # Seconds
-periodic_reset = 360  # with 60 second sleep, reset every 6 hours (just in case)
+sleep_interval = 15   # Seconds
+periodic_reset = 1440 # reset every 6 hours (21600 seconds) just in case / 15x1440 / 60x360
 
 
 def main():
